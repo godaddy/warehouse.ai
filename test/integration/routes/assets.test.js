1d633c14ad67dee3a31bca32aaef483c7b1815fb
Dustin Savery <dsavery@godaddy.com>
2018-03-07 16:34:25 -0800

    [tests] adding integration tests for assets endpoint

--- test/integration/routes/assets.test.js
+++ test/integration/routes/assets.test.js
@@ -22,22 +22,31 @@ function address(app, properties) {
 // TODO: Need testing config for publishing to s3 to store encrypted with
 // travis
 //
-describe.skip('/assets/*', function () {
-  var content = path.join(__dirname, 'builds.test.js'),
-    gzip = path.join(require('os').tmpdir(), 'build.test.js'),
+describe('/assets/*', function () {
+  var jsContent = path.join(__dirname, 'builds.test.js'),
+    jsGzip = path.join(require('os').tmpdir(), 'build.test.js'),
+    cssContent = path.join(__dirname, '..', '..', 'fixtures', 'payloads', 'my-styles.css'),
+    cssGzip = path.join(require('os').tmpdir(), 'my-styles.css'),
     spec = { name: 'pancake', version: '0.0.1', env: 'test' },
     publishOptions = {
       files: [{
-        content: content,
-        compressed: gzip,
+        content: jsContent,
+        compressed: jsGzip,
         fingerprint: '3x4mp311d',
         filename: 'builds.test.js',
         extension: '.js'
+      }, {
+        content: cssContent,
+        compressed: cssGzip,
+        fingerprint: '3x4mp311d',
+        filename: 'my-styles.css',
+        extension: '.css'
       }]
     },
     app;
 
-  fs.writeFileSync(gzip, zlib.gzipSync(fs.readFileSync(content)));
+  fs.writeFileSync(jsGzip, zlib.gzipSync(fs.readFileSync(jsContent)));
+  fs.writeFileSync(cssGzip, zlib.gzipSync(fs.readFileSync(cssContent)));
 
   before(function (next) {
     helpers.start({ http: 0 }, function (err, ret) {
@@ -64,14 +73,80 @@ describe.skip('/assets/*', function () {
   });
 
   describe('/assets/files/*', function () {
-    it('serves files for a given build');
-    it('responds with a 404 when appropriate');
-    it('accepts a filter to match filenames');
-    it('ignores case in filter parameters');
-    it('responds with 400 when filter is greater than 50 chars');
+    it('serves files for a given build', function (next) {
+      request(address(app, {
+        pathname: 'assets/files/pancake/test/0.0.1'
+      }), function (err, res, body) {
+        assume(err).doesnt.exist();
+        assume(res.statusCode).equals(200);
+        assume(res.headers['content-type']).equals('application/json; charset=utf-8');
+        const jsonBody = JSON.parse(body);
+        assume(jsonBody.files).exist();
+        assume(jsonBody.files).has.length(2);
+        assume(body.toString()).includes('/builds.test.js');
+
+        next();
+      });
+    });
+    it('responds with a 404 when appropriate', function (next) {
+      request(address(app, {
+        pathname: 'assets/files/pancake/test/0.0.2'
+      }), function (err, res) {
+        assume(err).doesnt.exist();
+        assume(res.statusCode).equals(404);
+
+        next();
+      });
+    });
+    it('accepts a filter to match filenames', function (next) {
+      request(address(app, {
+        pathname: 'assets/files/pancake/test/0.0.1',
+        query: { filter: 'css' }
+      }), function (err, res, body) {
+        assume(err).doesnt.exist();
+        assume(res.statusCode).equals(200);
+        assume(res.headers['content-type']).equals('application/json; charset=utf-8');
+        const jsonBody = JSON.parse(body);
+        assume(jsonBody.files).exist();
+        assume(jsonBody.files).has.length(1);
+        assume(body.toString()).includes('/my-styles.css');
+        assume(body.toString()).doesnt.includes('/builds.test.js');
+
+        next();
+      });
+    });
+    it('ignores case in filter parameters', function (next) {
+      request(address(app, {
+        pathname: 'assets/files/pancake/test/0.0.1',
+        query: { filter: 'CSS' }
+      }), function (err, res, body) {
+        assume(err).doesnt.exist();
+        assume(res.statusCode).equals(200);
+        assume(res.headers['content-type']).equals('application/json; charset=utf-8');
+        const jsonBody = JSON.parse(body);
+        assume(jsonBody.files).exist();
+        assume(jsonBody.files).has.length(1);
+        assume(body.toString()).includes('/my-styles.css');
+        assume(body.toString()).doesnt.includes('/builds.test.js');
+
+        next();
+      });
+    });
+    it('responds with 400 when filter is greater than 50 chars', function (next) {
+      request(address(app, {
+        pathname: 'assets/files/pancake/test/0.0.1',
+        query: { filter: 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz' }
+      }), function (err, res) {
+        assume(err).doesnt.exist();
+        assume(res.statusCode).equals(400);
+        assume(res.headers['content-type']).equals('application/json; charset=utf-8');
+
+        next();
+      });
+    });
   });
 
-  describe('/assets/:hash', function () {
+  describe.skip('/assets/:hash', function () {
     it('serves a safe 404 for css files', function (next) {
       request(address(app, {
         pathname: 'assets/ishouldexist.css'
@@ -105,7 +180,7 @@ describe.skip('/assets/*', function () {
         assume(err).doesnt.exist();
         assume(res.statusCode).equals(200);
         assume(res.headers['content-type']).includes('application/javascript');
-        assume(body).deep.equals(fs.readFileSync(content, 'utf8'));
+        assume(body).deep.equals(fs.readFileSync(jsContent, 'utf8'));
 
         next();
       });
@@ -122,7 +197,7 @@ describe.skip('/assets/*', function () {
         assume(res.statusCode).equals(200);
         assume(res.headers['content-type']).includes('application/javascript');
         assume(res.headers['content-encoding']).equals('gzip');
-        assume(body).deep.equals(fs.readFileSync(gzip, 'utf8'));
+        assume(body).deep.equals(fs.readFileSync(jsGzip, 'utf8'));
 
         next();
       });
