8d27ec8c9aea4c8f1869f0955e123cef048230ec
Jarrett Cruger <jcrugzz@gmail.com>
2018-03-22 17:05:14 -0400

    [api] integrate with authboot to handle bcrypt based basic auth check… (#23)
    * [api] integrate with authboot to handle bcrypt based basic auth checking for multiple users
    
    * [fix] finish authboot integration
    
    * [fix] console.log and spacing

--- test/integration/npm/econn.test.js
+++ test/integration/npm/econn.test.js
@@ -14,6 +14,7 @@ describe('npm routes', function () {
     helpers.integrationSetup({
       app: {
         http: 8092,
+        auth: false,
         npm: {
           urls: {
             read: 'http://npm.host.invalid',
