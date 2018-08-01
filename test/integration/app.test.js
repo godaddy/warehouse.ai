86a4bb56ded4456e1acf2e6b94a70513d83d0ec0
smehta <smehta@godaddy.com>
2018-08-01 14:06:25 -0700

    [wip] attempt to write a test

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -5,7 +5,9 @@ var assume = require('assume'),
   path = require('path'),
   hyperquest = require('hyperquest'),
   concat = require('concat-stream'),
-  registry = require('../../lib');
+  registry = require('../../lib'),
+  sinon = require('sinon'),
+  request = require('request');
 
 function address(app, properties) {
   const socket = app.servers.http.address();
@@ -15,6 +17,7 @@ function address(app, properties) {
     protocol: 'http'
   }, properties || {}));
 }
+
 var configFile = path.join(__dirname, '..', '..', 'config.example.json');
 describe('App (integration)', function () {
   it('should start with the given override port by default', function (done) {
@@ -73,6 +76,41 @@ describe('App (integration)', function () {
           assume(data.doc_count).is.a('number');
           app.close(done);
         }));
+    });
+  });
+
+  it.only('should log query params', function (done) {
+    var logs = [];
+    registry.start({
+      log: { level: 'critical' },
+      ensure: true,
+      auth: false,
+      config: {
+        file: configFile,
+        overrides: {
+          http: {
+            hostname: '127.0.0.1',
+            port: 0
+          }
+        }
+      }
+    }, function (err, app) {
+      assume(err).equals(null);
+      assume(app).truthy();
+      assume(app.log).is.an('object');
+
+      sinon.stub(app.log, 'info').callsFake((...msg) => logs.push(msg));
+      request(address(app, {
+        pathname: 'assets/files/pancake/test/0.0.1'
+      }), function (err, res, body) {
+        assume(err).doesnt.exist();
+        assume(res.statusCode).equals(200);
+
+        assume(logs.length).is.greaterThan(0);
+
+        app.close(done);
+      });
+
     });
   });
 });
