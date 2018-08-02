ee700a1e08d605178392f050f0ed68cc0cbc66b6
Sivan Mehta <Sivanmehta2@gmail.com>
2018-08-02 08:53:46 -0700

    Add morgan based logging (#28)

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -1,3 +1,4 @@
+/* eslint {max-nested-callbacks: 0} */
 'use strict';
 
 var assume = require('assume'),
@@ -5,7 +6,9 @@ var assume = require('assume'),
   path = require('path'),
   hyperquest = require('hyperquest'),
   concat = require('concat-stream'),
-  registry = require('../../lib');
+  registry = require('../../lib'),
+  sinon = require('sinon'),
+  request = require('request');
 
 function address(app, properties) {
   const socket = app.servers.http.address();
@@ -15,6 +18,7 @@ function address(app, properties) {
     protocol: 'http'
   }, properties || {}));
 }
+
 var configFile = path.join(__dirname, '..', '..', 'config.example.json');
 describe('App (integration)', function () {
   it('should start with the given override port by default', function (done) {
@@ -73,6 +77,57 @@ describe('App (integration)', function () {
           assume(data.doc_count).is.a('number');
           app.close(done);
         }));
+    });
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it('should log query params', function (done) {
+    var logs = [];
+    registry.start({
+      log: { level: 'critical' },
+      ensure: true,
+      auth: false,
+      config: {
+        file: configFile,
+        overrides: {
+          http: {
+            hostname: '127.0.0.1',
+            port: 0
+          }
+        }
+      }
+    }, function (err, app) {
+      assume(err).equals(null);
+      assume(app).truthy();
+      assume(app.log).is.an('object');
+
+      sinon.stub(app.log, 'info').callsFake((...msg) => logs.push(msg));
+      request(address(app, { pathname: 'healthcheck' }), function (healthCheckErr, res) {
+        assume(healthCheckErr).doesnt.exist();
+        assume(res.statusCode).equals(200);
+
+        var requestLogs = logs
+          .filter(l => l[0] === 'Request Served for wrhs')
+          .map(l => l[1]);
+
+        requestLogs.forEach(log => {
+          assume(log.request).exists();
+          assume(log.remote).exists();
+          assume(log.date).exists();
+          assume(log.referrer).exists();
+          assume(log['user-agent']).exists();
+          assume(log.env).exists();
+          assume(log.package).exists();
+          assume(log.version).exists();
+          assume(log.hash).exists();
+        });
+
+        app.close(done);
+      });
+
     });
   });
 });
